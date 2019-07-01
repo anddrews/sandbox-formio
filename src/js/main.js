50e94fcd67f37baf8fc0006aeaaacf49325a6a37
AndreiKandratsenka <andrei_kandratsenka@epam.com>
2019-07-01 10:37:04 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
 window.onload = function() {
   const node = document.querySelector('#test');
   const {formName, ...additionalData} = node.dataset;
-  Formio.createForm(node, `http://localhost:3001/${formName}`).then(form => {
-    form.submit = () => {
-      const {submit, ...data} = form.data;
-      const dataForSending = {...data, ...additionalData};
-      fetch('/ddd', {
-        method: 'post',
-        body: JSON.stringify(dataForSending),
-      })
-        .then(({location}) => {
-          window.location = '/typ.html'
-        });
-      console.log(dataForSending);
+  Formio.createForm(node, `http://localhost:3001/${formName}`, {
+    hooks: {
+      beforeSubmit: (submission, next) => {
+        submission.data = {...submission.data, ...additionalData};
+
+        next();
+      }
     }
-  });
+  })
 };
